dc7cc5760a7bf87838e8b85036f32fac60cf9233
Aakash.G <108251487+AakashG1329@users.noreply.github.com>
2024-03-19 12:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from deomainRegistration.routers import domain,user,hosting
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,28 +21,4 @@ app.add_middleware(
 app.include_router(user.router)
 app.include_router(domain.router)
 app.include_router(hosting.router)
-=======
-from fastapi import FastAPI
-from deomainRegistration.routers import domain,user,hosting
-from fastapi.middleware.cors import CORSMiddleware
 
-
-app=FastAPI()
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.include_router(user.router)
-app.include_router(domain.router)
-app.include_router(hosting.router)
->>>>>>> 02733a64f44d554f03dcf2da67bf91643dafecb8
