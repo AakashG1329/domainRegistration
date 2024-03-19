41af8e9bbfdf644b90bd146eb3c684918ddba48c
Aakash.G <108251487+AakashG1329@users.noreply.github.com>
2024-03-19 16:24:14 +0530

    Update database.py

--- deomainRegistration/database.py
+++ deomainRegistration/database.py
@@ -2,15 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-server = "gsmtechinfo.mssql.somee.com"
-user = "aakashg132906_SQLLogin_1"
-password = "8nxdn44l2q"
-db_name = "gsmtechinfo"
+# server = "gsmtechinfo.mssql.somee.com"
+# user = "aakashg132906_SQLLogin_1"
+# password = "8nxdn44l2q"
+# db_name = "gsmtechinfo"
 
 
 # Update the MySQL database URL
 # SQLALCHEMY_DATABASE_URL = "workstation id=gsmtechinfo.mssql.somee.com;packet size=4096;user id=aakashg132906_SQLLogin_1;pwd=8nxdn44l2q;data source=gsmtechinfo.mssql.somee.com;persist security info=False;initial catalog=gsmtechinfo;TrustServerCertificate=True"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost/domain_registration"
+#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost/domain_registration"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://if0_36192436:Lancy132906@sql207.infinityfree.com/if0_36192436_domain_registration"
 # SQLALCHEMY_DATABASE_URL =f'mssql+pyodbc://{user}:{password}@{server}/{db_name}?driver=ODBC Driver 17 for SQL Server'
 
 
